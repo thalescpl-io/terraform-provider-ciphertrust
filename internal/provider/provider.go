9f8aa132958385a8c6fbb5bc2f13c21ef1a2298e
Vijay DINESH-EXTERNAL S0127206 <vijay.dinesh@external.thalesgroup.com>
2024-12-03 20:58:25 +0530

    Added docs ,sample scripts, examples and UT

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -3,6 +3,8 @@ package provider
 import (
 	"context"
 	"fmt"
+	"github.com/thalescpl-io/terraform-provider-ciphertrust/internal/provider/connections"
+	"github.com/thalescpl-io/terraform-provider-ciphertrust/internal/provider/cte"
 	"os"
 
 	"github.com/google/uuid"
@@ -16,8 +18,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 	cm "github.com/thalescpl-io/terraform-provider-ciphertrust/internal/provider/cm"
 	common "github.com/thalescpl-io/terraform-provider-ciphertrust/internal/provider/common"
-	connections "github.com/thalescpl-io/terraform-provider-ciphertrust/internal/provider/connections"
-	cte "github.com/thalescpl-io/terraform-provider-ciphertrust/internal/provider/cte"
 )
 
 // Ensure the implementation satisfies the expected interfaces.
