9439f284113d0b73e5f2ae4e1feaddae94f55e95
om-shrirao <om.shrirao@external.thalesgroup.com>
2024-12-06 10:47:15 +0530

    Added HSM RoT Support for lunatct type (#18)
    * added example, sample-script and ut's for lunatct
    
    * skipped lunatct test
    
    * increased delay time for lunatct server
    
    * resolved review comments
    
    ---------
    
    Co-authored-by: himanshu.rana <himanshu.rana@external.thalesgroup.com>

--- examples/resources/ciphertrust_hsm_root_of_trust_setup/resource.tf
+++ examples/resources/ciphertrust_hsm_root_of_trust_setup/resource.tf
@@ -1,6 +1,6 @@
 # Terraform Configuration for CipherTrust Provider
 
-# These configurations demonstrate the creation of an HSM Root of trust setup for types "luna", "lunapci"
+# These configurations demonstrate the creation of an HSM Root of trust setup for types "luna", "lunapci" and "lunatct"
 # with the CipherTrust provider.
 
 # An example of HSM root of trust setup of type luna
@@ -44,6 +44,24 @@ resource "ciphertrust_hsm_root_of_trust_setup" "cm_hsm_rot_setup" {
   conn_info = {
     partition_name     = "kylo-partition"
     partition_password = "sOmeP@ssword"
+  }
+  reset = true
+  delay = 5
+}
+
+# An example of HSM root of trust setup of type lunatct
+resource "ciphertrust_hsm_root_of_trust_setup" "cm_hsm_rot_setup" {
+  type         = "lunatct"
+  conn_info = {
+    partition_name     = "kylo-partition"
+    partition_password = "sOmeP@ssword"
+  }
+  initial_config = {
+    host           = "172.20.32.11"
+    serial         = "1234"
+    server-cert    = "-----BEGIN CERTIFICATE-----\n...\n-----END CERTIFICATE-----"
+    client-cert    = "-----BEGIN CERTIFICATE-----\n...\n-----END CERTIFICATE-----"
+    client-cert-key = "-----BEGIN RSA PRIVATE KEY-----\n...\n-----END RSA PRIVATE KEY-----"
   }
   reset = true
   delay = 5
--- internal/provider/resource_hsm_rot_test.go
+++ internal/provider/resource_hsm_rot_test.go
@@ -78,3 +78,44 @@ resource "ciphertrust_hsm_root_of_trust_setup" "cm_hsm_rot_setup" {
 }
 
 // terraform destroy will perform automatically at the end of the test
+
+func TestResourceHSMRootOfTrustSetupLunatct(t *testing.T) {
+	// Remove skip after actual HSM data is used in test
+	t.Skip("Skipped!! dummy data in resource parameters")
+
+	// Create HSM RoT Setup with type "lunatct"
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{Config: providerConfig + `
+resource "ciphertrust_hsm_root_of_trust_setup" "cm_hsm_rot_setup" {
+  type         = "lunatct"
+  conn_info = {
+    partition_name     = "kylo-partition"
+    partition_password = "sOmeP@ssword"
+  }
+  initial_config = {
+    host           = "172.20.32.11"
+    serial         = "1234"
+    server-cert    = "-----BEGIN CERTIFICATE-----\n...\n-----END CERTIFICATE-----"
+    client-cert    = "-----BEGIN CERTIFICATE-----\n...\n-----END CERTIFICATE-----"
+    client-cert-key = "-----BEGIN RSA PRIVATE KEY-----\n...\n-----END RSA PRIVATE KEY-----"
+  }
+  reset = true
+  delay = 50
+}
+`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet("ciphertrust_hsm_root_of_trust_setup.cm_hsm_rot_setup", "id"),
+					resource.TestCheckResourceAttr("ciphertrust_hsm_root_of_trust_setup.cm_hsm_rot_setup", "type", "lunatct"),
+					resource.TestCheckResourceAttr("ciphertrust_hsm_root_of_trust_setup.cm_hsm_rot_setup", "config.%", "4"),
+					resource.TestCheckResourceAttr("ciphertrust_hsm_root_of_trust_setup.cm_hsm_rot_setup", "config.host", "172.20.32.11"),
+					resource.TestCheckResourceAttr("ciphertrust_hsm_root_of_trust_setup.cm_hsm_rot_setup", "config.partition_name", "kylo-partition"),
+					resource.TestCheckResourceAttr("ciphertrust_hsm_root_of_trust_setup.cm_hsm_rot_setup", "config.serial", "1234"),
+				),
+			},
+		},
+	})
+}
+
+// terraform destroy will perform automatically at the end of the test
