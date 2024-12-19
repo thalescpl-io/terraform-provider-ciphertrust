275d182643578ede1fdf69ecc56e74e6268c9795
Om Shrirao <om.shrirao@external.thalesgroup.com>
2024-12-19 12:07:25 +0530

    fix some issues in schema of cte usersets, processsets, signature set and resource set

--- internal/provider/cte/resource_cte_process_set.go
+++ internal/provider/cte/resource_cte_process_set.go
@@ -54,7 +54,7 @@ func (r *resourceCTEProcessSet) Schema(_ context.Context, _ resource.SchemaReque
 				Description: "The account which owns this resource.",
 				Computed:    true,
 			},
-			"dev_account ": schema.StringAttribute{
+			"dev_account": schema.StringAttribute{
 				Description: "The developer account which owns this resource's application.",
 				Computed:    true,
 			},
