59302806ecb008b660a44eeb4e5310445f7f6ae4
Vijay DINESH-EXTERNAL S0127206 <vijay.dinesh@external.thalesgroup.com>
2024-12-20 13:55:37 +0530

    Added UTs, ssample scripts, docs

---
+++ examples/data-sources/ciphertrust_scheduler/data-source.tf
@@ -0,0 +1,47 @@
+# Terraform Configuration for CipherTrust Provider
+
+# The provider is configured to connect to the CipherTrust appliance and fetch details
+# about the SCP (Secure Copy Protocol) connection.
+
+terraform {
+  # Specify required providers
+  required_providers {
+    ciphertrust = {
+      # Source location for the CipherTrust provider
+      source = "thalesgroup.com/oss/ciphertrust"
+      # Version of the provider to be used
+      version = "1.0.0"
+    }
+  }
+}
+
+# Configuration for the CipherTrust provider for authentication
+provider "ciphertrust" {
+  # The address of the CipherTrust appliance
+  # Replace with the actual address of your CipherTrust instance
+  address = "https://52.87.160.91"
+
+  # Username to authenticate against the CipherTrust appliance
+  username = "admin"
+
+  # Password to authenticate against the CipherTrust appliance
+  password = "SamplePassword@1"
+
+  bootstrap = "no"
+}
+
+# Data source for retrieving Scheduler Job Configs
+data "ciphertrust_scheduler_list" "jobs" {
+  filters = {
+  # Filters to narrow down the Scheduler Jobs
+    # The unique ID of the Job
+    id = "60f04cb1-4a48-4786-8965-39f2031518c4"
+  }
+  # Similarly can provide 'name' 'operation' 'disabled' etc to fetch the existing Scheduler Job
+  # Provide no filters and it will fetch all the scheduler jobs present in the provider
+}
+
+# Output the details of the Scheduler job
+output "scheduler_jobs" {
+  value = data.ciphertrust_scheduler_list.jobs
+}
